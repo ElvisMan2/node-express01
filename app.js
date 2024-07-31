0a53275ae9b18d60a1db670cf9587f3b54b7475a
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-07-31 18:27:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
-const express=require('express');
-const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-
-const PORT=process.env.PORT || 3000;
-const userRoutes=require('./routes/usersRoutes');//importa una función que define las rutas de la app
-const taskRoutes=require('./routes/tasksRoutes');//importa una función que define las rutas de la app
-
-const app=express();
-// Middleware para analizar datos JSON
-app.use(express.json());
-
-app.use(cors({
-    origin: '*', // Permitir todos los orígenes (puedes restringirlo a dominios específicos)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
-
-//agregando las ruta de usuarios
-userRoutes(app);
-//agrega las rutas de las tareas
-taskRoutes(app);
-
-
-//servir el frontend
-const path = require('path');
-// Servir archivos estáticos desde la carpeta public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta para servir la página de inicio (login y registro)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('Hello World!');
 });
 
-app.get('/todolist', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'todolist.html'));
-});
-
-app.listen(3000,()=>{
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
