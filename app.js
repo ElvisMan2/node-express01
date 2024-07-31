f71e537d9875ed8ae8efee245a7630a352148fbe
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-07-31 18:36:09 -0500

    rutas

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express=require('express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+
+const PORT=process.env.PORT || 3000;
+const userRoutes=require('./routes/usersRoutes');//importa una función que define las rutas de la app
+const taskRoutes=require('./routes/tasksRoutes');//importa una función que define las rutas de la app
+
+const app=express();
+// Middleware para analizar datos JSON
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Servidor corriendo en el puerto 3000');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+//agregando las ruta de usuarios
+userRoutes(app);
+//agrega las rutas de las tareas
+taskRoutes(app);
+
+app.listen(3000,()=>{
+    console.log('Servidor corriendo en el puerto 3000');
 });
